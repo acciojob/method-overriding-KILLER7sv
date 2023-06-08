a9ecea0884b6c593a6c7ef4007587083e8292e52
KILLER7sv <116873144+KILLER7sv@users.noreply.github.com>
2023-06-08 20:52:37 +0530

    Completed solution

--- src/main/java/com/driver/B.java
+++ src/main/java/com/driver/B.java
@@ -4,8 +4,4 @@ public class B extends A{
     public String meth(){
         return "Method is overridden in Extended class B";
     }
-    public static void main(String[] args) {
-        B e = new B();
-        System.out.println(e.meth());
-    }
 }
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,8 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        B e = new B();
+        System.out.println(e.meth());
+    }
 }
